6a7f7281497696118e3ac574f0674dc94fc3c807
Jakub Marek Jaworski <jakubmjaworskikontakt@gmail.com>
2024-12-10 23:58:28 +0100

    Add Reactive Form and validation

--- salon-samochodowy-frontend/src/app/components/show-car-form/show-car-form.component.ts
+++ salon-samochodowy-frontend/src/app/components/show-car-form/show-car-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, Input } from '@angular/core';
 import { Car } from '../../services/car.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
 
 /**
  * @module ShowCarForm
@@ -34,12 +35,12 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'show-car-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './show-car-form.component.html',
   styleUrls: ['./show-car-form.component.css'],
 })
 export class ShowCarForm {
-  
+  showcarform: FormGroup;
   /**
    * @input
    * @type {Car}
@@ -67,10 +68,20 @@ export class ShowCarForm {
    * Inicjalizuje komponent, przypisując przekazane dane do właściwości `car`.
    */
   constructor(
+    private fb: FormBuilder,
     private dialogRef: MatDialogRef<ShowCarForm>,
     @Inject(MAT_DIALOG_DATA) data: Car
   ) {
     Object.assign(this.car, data);
+    this.showcarform = this.fb.group({
+      brand: [this.car.brand, Validators.required],
+      model: [this.car.model, Validators.required],
+      year: [this.car.year, [Validators.required, Validators.min(1886), Validators.max(new Date().getFullYear())]],
+      vin: [this.car.vin,[Validators.required, Validators.pattern(/^[A-HJ-NPR-Z0-9]{17}$/)]],//Dozwolone znaki w walidacji: Duże litery od A do Z z wyłączeniem I oraz O oraz liczby
+      price:[this.car.price, [Validators.required, Validators.min(0)]],
+      horsePower: [this.car.horsePower, [Validators.required, Validators.min(1)]],
+      isAvailableForRent: [this.car.isAvailableForRent]
+    })
   }
 
   /**
@@ -85,6 +96,13 @@ export class ShowCarForm {
    * ```
    */
   addCar() {
+    this.car.brand = this.showcarform.get('brand')!.value;
+    this.car.model = this.showcarform.get('model')!.value;
+    this.car.year = this.showcarform.get('year')!.value;
+    this.car.vin = this.showcarform.get('vin')!.value;
+    this.car.price = this.showcarform.get('price')!.value;
+    this.car.horsePower = this.showcarform.get('horsePower')!.value;
+    this.car.isAvailableForRent = this.showcarform.get('isAvailableForRent')!.value;
     this.dialogRef.close(this.car);
   }
 
